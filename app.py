dca52c8a7ed90211cb446244ad4a4f4d95a05ed3
Princess <komalrao.vds@gmail.com>
2025-04-03 22:46:45 +0530

    Updated project with latest fixes

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import google.generativeai as genai
 # images = convert_from_path("sample.pdf", poppler_path="C:\\Program Files (x86)\\poppler\\Library\\bin")
 
 
-POPPLER_PATH = "/usr/bin"  # Poppler is installed in this directory on Linux (Streamlit Cloud)
+POPPLER_PATH = "C:\\Program Files (x86)\\poppler\\Library\\bin"  # Poppler is installed in this directory on Windows (Streamlit Cloud)
 
 
 
